f75755d998ec2291364f2918eb37e3eb83db7874
a8568730 <a8568730@hotmail.com>
2018-12-18 18:13:55 +0800

    * build_deb.sh, cp LICENSE

--- build_deb.sh
+++ build_deb.sh
@@ -8,6 +8,7 @@ mkdir -p deb/DEBIAN/
 mkdir -p deb/usr/share/rime-data/
 cp ./control deb/DEBIAN/
 cp ./taigi.schema.yaml ./taigi.dict.yaml deb/usr/share/rime-data/
+cp ./LICENSE deb/DEBIAN/copyright
 
 # Build pkg
 dpkg -b deb/ rime-itaigi.deb
